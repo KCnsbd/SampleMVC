91917dbfab433f54c9d98ec6487f966916753cb6
KeanaClarisse <s2023102074@firstasia.edu.ph>
2025-02-08 13:38:26 +0800

    Add project files.

---
+++ SampleMVC/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index1()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
